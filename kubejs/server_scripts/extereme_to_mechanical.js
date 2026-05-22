8bb73a915208958d91f29faf55b50cbc9ad60313
walksanator <walkerffo22@gmail.com>
2026-05-22 13:33:18 -0700

    creatify avarita recipes.

--- kubejs/server_scripts/extereme_to_mechanical.js
+++ kubejs/server_scripts/extereme_to_mechanical.js
@@ -1,12 +1,34 @@
 
 ServerEvents.recipes(event => {
-  // You can replace `event` with any name you like, as
-  // long as you change it inside the callback too!
+    // You can replace `event` with any name you like, as
+    // long as you change it inside the callback too!
 
-  event.getRecipes()
-  // This part, inside the curly braces, is the callback.
-  // You can modify as many recipes as you like in here,
-  // without needing to use ServerEvents.recipes() again.
+    event.forEachRecipe({ type: "avaritia:shaped_table" }, r => {
+        const avarita = JSON.parse(r.json.toString())
+        const create = {
+            "type": "create:mechanical_crafting",
+            "key": avarita.key,
+            "pattern": avarita.pattern,
+            "result": avarita.result,
+            "accept_mirrored": false,
+            "neoforge:conditions": avarita["neoforge:conditions"]
+        }
+        event.custom(create)
+    })
 
-  console.log('Hello! The recipe event has fired!')
+    event.forEachRecipe({ type: "avaritia:shapeless_table" }, r => {
+        const avarita = JSON.parse(r.json.toString())
+        const create = {
+            "type": "create:mixing",
+            "ingredients": r.ingredients,
+            "heat_requirement": "superheated",
+            "results": [r.result],
+        }
+        event.custom(create)
+    })
+    // This part, inside the curly braces, is the callback.
+    // You can modify as many recipes as you like in here,
+    // without needing to use ServerEvents.recipes() again.
+
+    console.log('Hello! The recipe event has fired!')
 })
